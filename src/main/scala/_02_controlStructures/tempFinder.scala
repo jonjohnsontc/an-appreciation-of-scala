c927ac8999f09d94b43d2b85ad2b1b0e627d98b7
Jon Johnson <jonjohnsontc@live.com>
2024-09-10 17:41:35 -0700

    checkpoint

--- src/main/scala/_02_controlStructures/tempFinder.scala
+++ src/main/scala/_02_controlStructures/tempFinder.scala
@@ -1,14 +1,16 @@
 package gay.jons
 package _02_controlStructures
 
+import scala.util.Random
+//import java.util.Random
+
 // positional arguments can be passed through to main as normal
 // and will be interpreted as the type you provide
 // that seems pretty nifty for small scripts
-@main def findTemps(city: String, state: Int): Unit =
-  if city == "Denver" then
-    println("37.5 degrees celsius")
-    println(s"And here's what I think about your $state, fuckboy")
-  else if city == "Pawnee" then
-    println("21 degrees celsius")
-  else
-    println("Move somewhere else bub")
+@main def findTemps(city: String): Unit =
+// scala version
+  val temp = Random.between(0.0, 100)
+//  java version
+//  val random = Random()
+//  val temp = random.nextFloat() * 100
+  println(f"Current temp in $city%s: $temp%.2f°")
