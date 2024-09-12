58afc63da0954561ede10d5fa3bf809b9bfb47bf
Jon Johnson <jonjohnsontc@live.com>
2024-09-11 18:14:11 -0700

    looking around the scala api docs

---
+++ src/main/scala/_01_helloWorld/explore.sc
@@ -0,0 +1,4 @@
+// adding colors to output
+import io.AnsiColor._
+println(s"${BOLD}${MAGENTA}Hey cutie")
+println("Here's another line")
--- src/main/scala/_01_helloWorld/userInput.scala
+++ src/main/scala/_01_helloWorld/userInput.scala
@@ -2,9 +2,10 @@ package gay.jons
 package _01_helloWorld
 
 import scala.io.StdIn.readLine
+import io.AnsiColor._
 
 @main def helloInteractive(): Unit =
   println("Please enter your name")
   val name: String = readLine()
-
-  println("Hello, " + name + "!")
+  println(s"Hello, ${BOLD}${MAGENTA}${name}!${RESET}")
+  println("I'm glad you came")
