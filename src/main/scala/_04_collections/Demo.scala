53cc8c0d915d91d66fb0c946bb4dcdf4ba9407ed
Jon Johnson <jonjohnsontc@live.com>
2024-09-12 21:12:27 -0700

    checkpoint

--- src/main/scala/_04_collections/Demo.scala
+++ src/main/scala/_04_collections/Demo.scala
@@ -1,10 +1,16 @@
 package gay.jons
 package _04_collections
 
-import scala.::
-
 // Scala has heterogenous collections, wonder what that looks like decompiled
-val heteroArr: List[String | Double] = "this" :: 8.8 :: "is" :: 99.3 :: "a list" :: Nil
+val heteroList: List[String | Double] = "this" :: 8.8 :: "is" :: 99.3 :: "a list" :: Nil
+type GarbagePlate = Boolean | String | Double | Int
 
 @main def demo(): Unit =
-  for (item <- heteroArr) do println(item)
+  val more = heteroList ::: "more" :: 99.21 :: "linkedList" :: 21.11 :: "nonsense" :: Nil
+  val stillMore = more ::: more
+  val lazyList = LazyList(false, false, true, true, false, true)
+  val andThisVec = Vector(2, 2, 3, 6, 7, 19)
+
+  val lookAtThisShit = lazyList.concat(more)
+  val bigPile: Vector[GarbagePlate] = andThisVec.concat(lookAtThisShit)
+  for item <- bigPile do println(s"${item}, type: ${item.getClass}")
