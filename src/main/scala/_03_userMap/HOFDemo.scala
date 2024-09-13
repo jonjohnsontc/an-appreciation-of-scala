577f4168fe7f0715f53ef8739a7dfb50d52824d9
Jon Johnson <jonjohnsontc@live.com>
2024-09-12 19:15:29 -0700

    checkpoint

--- src/main/scala/_03_userMap/HOFDemo.scala
+++ src/main/scala/_03_userMap/HOFDemo.scala
@@ -1,24 +1,25 @@
 package gay.jons
 package _03_userMap
 
-import java.nio.file.Path
-import java.nio.file.Files
+import java.nio.file.{Path, Files}
 import scala.jdk.CollectionConverters.*
 
-object HOFDemo {
+object HOFDemo:
   def map[A, B](f: (B) => A, xs: List[B]): List[A] =
     for x <- xs yield f(x)
 
   def makeCute(filepath: String | Path): String =
     val len = filepath.toString.length
-    val padding = StringBuilder()
+    val padLeft = StringBuilder()
+    val padRight = StringBuilder()
     val padLength = 76 - len
-    for (_ <- 0 to padLength) do padding += ' '
-    s"❤ ${filepath}${padding} ❤"
+    for (_ <- 0 to 32) do padLeft += ' '
+    for (_ <- 33 to padLength) do padRight += ' '
+    s"❤ ${padLeft}${filepath}${padRight} ❤"
 
   def main(args: Array[String]): Unit =
     val dir: List[Path] = Files.list(Path.of(".")).toList.asScala.toList
     val cuteDir = map(makeCute, dir)
     println("Here's the contents of your directory")
+    println()
     for file <- cuteDir do println(file)
-}
